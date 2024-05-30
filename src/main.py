568cd5b1f221bc453b7805bb64d70001b38356c4
Cloudgen Wong <cloudgen@CloudgendeMacBook-Pro.local>
2024-05-30 13:54:51 +0800

    Update to v1.0

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def main():
   with open("/tmp/test.txt","a") as f:
-    f.write(f"Started at: {datetime.now()}\n")
+    f.write(f"Started (v1.0) at: {datetime.now()}\n")
 
 if __name__=='__main__':
   main()
